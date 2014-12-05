90c57a87c7ea595f01309710a9f53208ec08645f
Trystan Spangler <trystan.s@comcast.net>
2014-12-04 19:36:56 -0800

    initial commit

---
+++ test/super_simple_window/core_test.clj
@@ -0,0 +1,7 @@
+(ns super-simple-window.core-test
+  (:require [clojure.test :refer :all]
+            [super-simple-window.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
