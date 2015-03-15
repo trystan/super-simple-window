b1b21671927ac3bcb170f41dd3fab221895db536
Trystan Spangler <trystan.s@comcast.net>
2015-03-15 13:07:04 -0700

    use timer if :on-timer is present. add :on-mouse-move and :on-mouse-click callbacks.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject super-simple-window "0.1.0"
+(defproject super-simple-window "0.1.1"
   :description "Create and return a Canvas within a JFrame using a bunch of optional settings and callbacks."
   :url "https://github.com/trystan/super-simple-window"
   :license {:name "Eclipse Public License"
